6b354b99b8c0f608d77c9e3b2d508e69fce35542
Yevhenii Huselietov <d46k16@gmail.com>
2017-11-09 06:03:20 +0200

    Fix chmod

--- tools/tstree/index.js
+++ tools/tstree/index.js
@@ -1,228 +1,160 @@
 #!/usr/bin/env node
-import * as ts from "typescript";
-import * as glob from "glob";
-import * as fs from 'fs';
-
-interface MetadataDictionary
-{
-    [id: string]: MetadataEntry;
-}
-
-interface MetadataEntry
-{
-    name?: string;
-    fileName?: string;
-    moduleName?: string;
-    parentClass?: string;
-    children: MetadataDictionary;
-    text: string;
-}
-
+"use strict";
+exports.__esModule = true;
+var ts = require("typescript");
+var glob = require("glob");
+var fs = require("fs");
 /** Generate documentation for all classes in a set of .ts files */
-function generateClassMetadata(fileNames: string[], options: ts.CompilerOptions): MetadataDictionary
-{
+function generateClassMetadata(fileNames, options) {
     // Build a program using the set of root file names in fileNames
-    let program = ts.createProgram(fileNames, options);
-
+    var program = ts.createProgram(fileNames, options);
     // Get the checker, we will use it to find more about classes
-    let checker = program.getTypeChecker();
-
-    let output: MetadataDictionary = {};
-
+    var checker = program.getTypeChecker();
+    var output = {};
     // Visit every sourceFile in the program
-    for (const sourceFile of program.getSourceFiles()) {
+    for (var _i = 0, _a = program.getSourceFiles(); _i < _a.length; _i++) {
+        var sourceFile = _a[_i];
         // Walk the tree to search for classes
         ts.forEachChild(sourceFile, visit);
     }
-
     return output;
-
     /** visit nodes finding exported classes */
-    function visit(node: ts.Node)
-    {
-        const path = node.getSourceFile()['path'].toString();
-
+    function visit(node) {
+        var path = node.getSourceFile()['path'].toString();
         if (path.includes('node_modules')) {
             return;
         }
-
         if (node.kind === ts.SyntaxKind.ClassDeclaration) {
             // This is a top level class, get its symbol
-            let symbol = checker.getSymbolAtLocation((<ts.ClassDeclaration>node).name);
-            let classMetadata = serializeClass(symbol);
-
+            var symbol = checker.getSymbolAtLocation(node.name);
+            var classMetadata = serializeClass(symbol);
             classMetadata.text = node.getSourceFile().text;
-
-            const classFqcn = getFqcnBySymbol(symbol);
+            var classFqcn = getFqcnBySymbol(symbol);
             classMetadata.moduleName = classFqcn.join('.');
-
-            if ((<ts.ClassDeclaration>node).heritageClauses) {
-                for (let heritage of (<ts.ClassDeclaration>node).heritageClauses) {
+            if (node.heritageClauses) {
+                for (var _i = 0, _a = node.heritageClauses; _i < _a.length; _i++) {
+                    var heritage = _a[_i];
                     if (heritage.token === ts.SyntaxKind.ExtendsKeyword) {
-
-                        let identifier;
-
+                        var identifier = void 0;
                         if (heritage) {
                             identifier = heritage.types[0].expression;
                         }
-
-                        let heritageSymbol = checker.getSymbolAtLocation(identifier);
-
+                        var heritageSymbol = checker.getSymbolAtLocation(identifier);
                         if (!heritageSymbol) {
                             if (identifier.kind === ts.SyntaxKind.Identifier) {
                                 classMetadata.parentClass = classFqcn.slice(0, -1).join('.') + '.' + identifier.text;
-                            } else {
+                            }
+                            else {
                                 classMetadata.parentClass = getFqcnByPropertyAccess(identifier);
                             }
-                        } else {
+                        }
+                        else {
                             classMetadata.parentClass = getFqcnBySymbol(heritageSymbol).join('.');
                         }
                     }
                 }
             }
-
             output[classMetadata.moduleName] = classMetadata;
         }
-
         ts.forEachChild(node, visit);
     }
-
     /** Serialize a symbol into a json object */
-    function serializeClass(symbol: ts.Symbol): MetadataEntry
-    {
+    function serializeClass(symbol) {
         return {
             name: symbol.getName(),
             text: '',
             children: {}
         };
     }
-
-    function getFqcnBySymbol(symbol: ts.Symbol): string[]
-    {
-        let fqcn = [];
-        let previous = symbol;
-
+    function getFqcnBySymbol(symbol) {
+        var fqcn = [];
+        var previous = symbol;
         if (previous && previous.declarations[0].kind === ts.SyntaxKind.ImportEqualsDeclaration) {
             return getFqcnByQN(previous.declarations[0]['moduleReference']).split('.');
         }
-
         while (true) {
             fqcn.unshift(previous.name);
             previous = previous['parent'];
-
             if (!previous) {
                 break;
             }
         }
-
         fqcn.shift();
-
         return fqcn;
     }
-
-    function getFqcnByQN(qn: ts.QualifiedName)
-    {
-        let fqcn = [];
-
-        const pickIt = (qn: ts.QualifiedName) => {
+    function getFqcnByQN(qn) {
+        var fqcn = [];
+        var pickIt = function (qn) {
             if (!qn.left.hasOwnProperty('text')) {
-                pickIt(<ts.QualifiedName> qn.left);
-            } else {
+                pickIt(qn.left);
+            }
+            else {
                 fqcn.unshift(qn.left['text']);
             }
-
             fqcn.push(qn.right.text);
         };
-
         pickIt(qn);
-
         return fqcn.join('.');
     }
-
-    function getFqcnByPropertyAccess(property: ts.PropertyAccessExpression)
-    {
-        let fqcn = [];
-
-        const pickIt = (qn: ts.PropertyAccessExpression) => {
+    function getFqcnByPropertyAccess(property) {
+        var fqcn = [];
+        var pickIt = function (qn) {
             if (qn.hasOwnProperty('expression')) {
-                pickIt(<ts.PropertyAccessExpression> qn.expression);
-            } else {
+                pickIt(qn.expression);
+            }
+            else {
                 fqcn.unshift(qn['text']);
             }
-
             if (qn.hasOwnProperty('name')) {
                 fqcn.push(qn.name.text);
             }
         };
-
         pickIt(property);
-
         return fqcn.join('.');
     }
 }
-
-
-const globInstance = new glob.GlobSync(process.argv.slice(2, -1).toString() + '/**/*.ts');
-
-const metadata = generateClassMetadata(globInstance.found, {
+var globInstance = new glob.GlobSync(process.argv.slice(2, -1).toString() + '/**/*.ts');
+var metadata = generateClassMetadata(globInstance.found, {
     target: ts.ScriptTarget.ESNext, module: ts.ModuleKind.CommonJS
 });
-
-function hasChildren(moduleNameSearch, metadata: MetadataDictionary)
-{
-    for (let moduleName in metadata) {
-        const entry = metadata[moduleName];
-
+function hasChildren(moduleNameSearch, metadata) {
+    for (var moduleName in metadata) {
+        var entry = metadata[moduleName];
         if (entry.parentClass === moduleNameSearch) {
             return true;
         }
     }
-
     return false;
 }
-
-function unflattenMetadata(metadata: MetadataDictionary)
-{
-    for (let moduleName in metadata) {
-        const entry = metadata[moduleName];
-
+function unflattenMetadata(metadata) {
+    for (var moduleName in metadata) {
+        var entry = metadata[moduleName];
         if (!hasChildren(entry.moduleName, metadata)) {
-            const parent = metadata[entry.parentClass];
-
-            if (!parent) {
+            var parent_1 = metadata[entry.parentClass];
+            if (!parent_1) {
                 continue;
             }
-
-            parent.children[moduleName] = entry;
+            parent_1.children[moduleName] = entry;
             delete metadata[moduleName];
-
             unflattenMetadata(metadata);
         }
     }
 }
-
-function concatSources(metadata: MetadataDictionary)
-{
-    const data = [];
-
-    for (let moduleName in metadata) {
-        const entry = metadata[moduleName];
-
+function concatSources(metadata) {
+    var data = [];
+    for (var moduleName in metadata) {
+        var entry = metadata[moduleName];
         data.push(entry.text);
         data.push(concatSources(entry.children));
     }
-
     return data.join('');
 }
-
 unflattenMetadata(metadata);
-
-const sources = concatSources(metadata);
-const transpiled = ts.transpileModule(sources, {
+var sources = concatSources(metadata);
+var transpiled = ts.transpileModule(sources, {
     compilerOptions: {
         module: ts.ModuleKind.CommonJS,
         target: ts.ScriptTarget.ES5
     }
 });
-
 fs.writeFileSync(process.argv.slice(3).toString(), transpiled.outputText);
